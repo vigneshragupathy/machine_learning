88004becb72e5f2328ee107f433ae8bbdb84b9ea
vikki <me@vikki.in>
2016-02-01 15:35:32 +0530

    added

--- document_classify/vikki.py
+++ document_classify/vikki.py
@@ -10,6 +10,7 @@ def remove_punctuation(s):
 def tokenize(text):
     #text = remove_punctuation(text)
     text = text.lower()
+    #return text
     return re.split("\W+", text)
 
 def count_words(words):
@@ -18,8 +19,8 @@ def count_words(words):
         wc[word] = wc.get(word, 0.0) + 1.0
     return wc
 
-#s = "Hello my name, is Greg. My favorite food is pizza."
-#count_words(tokenize(s))
+#s = "HTTP GET /?gfe_rd=cr&ei=yempVvzeDrLG8Aez97WICQ HTTP/1.1"
+#print count_words(tokenize(s))
 
 from sh import find
 
@@ -27,11 +28,11 @@ from sh import find
 vocab = {}
 word_counts = {
     "http": {},
-    "https": {}
+    "ssl": {}
 }
 priors = {
     "http": 0.,
-    "https": 0.
+    "ssl": 0.
 }
 docs = []
 for f in find("sample-data"):
@@ -42,13 +43,12 @@ for f in find("sample-data"):
     elif "http" in f:
         category = "http"
     else:
-        category = "https"
+        category = "ssl"
     docs.append((category, f))
     # ok time to start counting stuff...
     priors[category] += 1
     text = open(f).read()
     words = tokenize(text)
-    #print words
     #print words
     counts = count_words(words)
     #print counts
@@ -62,46 +62,41 @@ for f in find("sample-data"):
         word_counts[category][word] += count
         #print vocab[word]
         #print word_counts[category][word]
-
-new_doc = open("examples/dummy_3.txt").read()
+new_doc = open("examples/sample.txt").read()
 words = tokenize(new_doc)
-print words
 counts = count_words(words)
-print counts
 
 import math
 
 prior_http = (priors["http"] / sum(priors.values()))
-prior_https = (priors["https"] / sum(priors.values()))
-print prior_http
-print prior_https
+prior_ssl = (priors["ssl"] / sum(priors.values()))
 
 log_prob_http = 0.0
-log_prob_https = 0.0
+log_prob_ssl = 0.0
 for w, cnt in list(counts.items()):
     # skip words that we haven't seen before, or words less than 3 letters long
-    if w not in vocab or len(w) <= 1:
+    if w not in vocab or len(w) <= 2:
         continue
 
     p_word = vocab[w] / sum(vocab.values())
+    #print p_word
+    #print word_counts["http"].get(w, 0.0)
+    #print sum(word_counts["http"].values())
     p_w_given_http = word_counts["http"].get(w, 0.0) / sum(word_counts["http"].values())
-    p_w_given_https = word_counts["https"].get(w, 0.0) / sum(word_counts["https"].values())
+    p_w_given_ssl = word_counts["ssl"].get(w, 0.0) / sum(word_counts["ssl"].values())
 
     if p_w_given_http > 0:
         log_prob_http += math.log(cnt * p_w_given_http / p_word)
-    if p_w_given_https > 0:
-        log_prob_https += math.log(cnt * p_w_given_https / p_word)
+    if p_w_given_ssl > 0:
+        log_prob_ssl += math.log(cnt * p_w_given_ssl / p_word)
 
 #print("Score(http)  :", math.exp(log_prob_http + math.log(prior_http)))
-#print("Score(https):", math.exp(log_prob_https + math.log(prior_https)))
+#print("Score(ssl):", math.exp(log_prob_ssl + math.log(prior_ssl)))
 
 score_http = math.exp(log_prob_http + math.log(prior_http))
-score_https = math.exp(log_prob_https + math.log(prior_https))
-print "Score for http is %s"%(score_http)
-print "Score for https is %s"%(score_https)
+score_ssl =  math.exp(log_prob_ssl + math.log(prior_ssl))
 
-
-if score_http > score_https:
-    print "The file is classified as http"
+if score_http > score_ssl:
+    print "The traffic is classified as http"
 else:
-    print "The file is classified as https"
+    print "The traffic is classifed as https(ssl)"
